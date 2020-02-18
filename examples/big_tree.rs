89c0448b80b61b9a12a60a3b032919bf8200088c
Robert Usher <266585+dcchut@users.noreply.github.com>
2020-02-18 21:53:26 +1100

    Add big tree example

--- examples/big_tree.rs
+++ examples/big_tree.rs
@@ -1,7 +1,6 @@
 use anyhow::Result;
 use rand::Rng;
 
-use spaceindex::rtree::rendering::graphviz::render_gviz;
 use spaceindex::rtree::rendering::image::TreeRenderOptions;
 use spaceindex::rtree::RTree;
 
@@ -14,7 +13,8 @@ fn main() -> Result<()> {
     let mut tree = RTree::new(2);
     let mut rng = rand::thread_rng();
 
-    for _ in 0..100 {
+    // create a <really really big> tree.
+    for _ in 0..500_000 {
         let xmin = rng.gen_range(0.0, RENDER_WIDTH as f64);
         let ymin = rng.gen_range(0.0, RENDER_HEIGHT as f64);
         let height = rng.gen_range(0.1, MAX_REGION_SIDE_LENGTH);
@@ -26,32 +26,10 @@ fn main() -> Result<()> {
 
     tree.validate_consistency();
 
-    // render a graphviz representation of the RTree
-    render_gviz(&tree, "tree.dot");
-
-    let mut render_options = TreeRenderOptions::new(RENDER_WIDTH, RENDER_HEIGHT);
-
-    // draw out each child of the root node layer-by-layer
-    for (ix, child_index) in tree.root_node().child_index_iter().enumerate() {
-        // draw it all out
-        for threshold in 0..5 {
-            render_options.with_threshold(threshold).draw_tree(
-                format!("Tree_C{}_T{}.png", ix, threshold),
-                &tree,
-                child_index,
-            );
-        }
-    }
-
     // do a global rendering
-    render_options
+    TreeRenderOptions::new(RENDER_WIDTH, RENDER_HEIGHT)
         .without_threshold()
         .draw_tree("Tree.png", &tree, tree.root_index());
-
-    println!(
-        "{} children of the root node",
-        tree.root_node().child_count()
-    );
 
     Ok(())
 }
