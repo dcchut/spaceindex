5daf6d2244bb54983c47c4fc64094a3f020be6cb
Robert Usher <266585+dcchut@users.noreply.github.com>
2020-02-21 22:33:50 +1100

    Again

--- ci/travis/test.sh
+++ ci/travis/test.sh
@@ -5,6 +5,5 @@ set -ex
 cargo +nightly build --verbose
 
 # Only test spaceindex - for some reason we get link errors testing spaceindex-py
-pushd spaceindex
+cd ../../../spaceindex/
 cargo +nightly test --verbose
-popd
