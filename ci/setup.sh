b56b1bb6db6d16107a7d730b4f143eaa2069945b
Robert Usher <266585+dcchut@users.noreply.github.com>
2021-01-17 13:00:40 +1100

    Update deps (#34)
    * Update deps
    
    * bump up to py3.6 min req
    
    * fmt
    
    * Setup
    
    * run
    
    * rust already installed
    
    * use nightly
    
    * install rustfmt
    
    * add clippy too
    
    * into -> from
    
    * Fix tests
    
    * Try this
    
    * need these components
    
    * Split these out into separate steps
    
    * give args
    
    * test this explicitly
    
    * use relative path instead
    
    * specify another level
    
    * checkout the repo :/
    
    * use x64 arch
    
    * update some commands
    
    * Don't run the tests for now

--- ci/setup.sh
+++ ci/setup.sh
@@ -2,21 +2,9 @@
 
 set -e
 
-# Find the installed version of a binary, if any
-_installed() {
-    VERSION=$($@ --version 2>/dev/null || echo "$@ none")
-    echo $VERSION | rev | cut -d' ' -f1 | rev
-}
-
-# Find the latest available version of a binary on `crates.io`
-_latest() {
-    VERSION=$(cargo search -q "$@" | grep "$@" | cut -f2 -d"\"")
-    echo $VERSION
-}
-
-### Setup Rust toolchain #######################################################
-curl -SsL "https://sh.rustup.rs/" | sh -s -- -y --default-toolchain=$TRAVIS_RUST_VERSION
-export PATH=$PATH:$HOME/.cargo/bin
+# Get rustfmt goin
+rustup component add rustfmt
+rustup component add clippy
 
 ### Setup python linker flags ##################################################
 
